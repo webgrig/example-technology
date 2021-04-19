623f239274b9094552f005c632a11bb3addc9ee6
webgrig <grigoriev.ua@gmail.com>
2021-04-19 17:54:58 +0200

    parce xml

--- app/Http/Controllers/API/CurrencyController.php
+++ app/Http/Controllers/API/CurrencyController.php
@@ -5,11 +5,10 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\Currency;
 use Illuminate\Http\Request;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Redis;
 
-class CurrencyController extends Controller
-{
+class CurrencyController extends Controller{
     /**
      * Display a listing of the resource.
      *
@@ -22,8 +21,13 @@ class CurrencyController extends Controller
         $firstDateAPI = '1999-01-04';
         $amountFirstDateAPI = 27;
         $reader = new \XMLReader();
-        if (!$reader->open("https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist.xml")) {
-            die("Failed to open 'eurofxref-hist.xml'");
+        $file = 'https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist.xml';
+        $newFile = storage_path()  . '\\' . basename($file);
+        if (!copy($file, $newFile)){
+            die("Failed to copy remote file " . basename($file));
+        }
+        elseif (!$reader->open($newFile)) {
+            die("Failed to open " . basename($file));
         }
         while ($reader->read() && $reader->localName !== 'Cube') {
             continue;
@@ -63,55 +67,56 @@ class CurrencyController extends Controller
             else{$reader->next();}
         }
         $reader->close();
+        unlink($newFile);
         echo 'OK';
     }
 
-        /**
-         * Store a newly created resource in storage.
-         *
-         * @param \Illuminate\Http\Request $request
-         * @return \Illuminate\Http\Response
-         */
-        public
-        function store(Request $request)
-        {
-            //
-        }
-
-        /**
-         * Display the specified resource.
-         *
-         * @param int $id
-         * @return \Illuminate\Http\Response
-         */
-        public
-        function show($id)
-        {
-            //
-        }
-
-        /**
-         * Update the specified resource in storage.
-         *
-         * @param \Illuminate\Http\Request $request
-         * @param int $id
-         * @return \Illuminate\Http\Response
-         */
-        public
-        function update(Request $request, $id)
-        {
-            //
-        }
-
-        /**
-         * Remove the specified resource from storage.
-         *
-         * @param int $id
-         * @return \Illuminate\Http\Response
-         */
-        public
-        function destroy($id)
-        {
-            //
-        }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public
+    function store(Request $request)
+    {
+        //
     }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public
+    function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public
+    function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public
+    function destroy($id)
+    {
+        //
+    }
+}
